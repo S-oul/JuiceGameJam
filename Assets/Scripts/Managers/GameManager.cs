75471f6a8c3c8c27aaa18562f6837cfc8c0275cb
Sacha (Pc) <S.soulslasher@gmail.com>
2025-02-06 22:41:25 +0100

    font fix

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -95,7 +95,7 @@ public class GameManager : MonoBehaviour
     {
         while (Time.timeScale > 0)
         {
-            Time.timeScale -= Time.deltaTime/2;
+            Time.timeScale -= Time.deltaTime;
             yield return null;
         }
 
